3e5ee0d1fb6a98b7a1cb5fc67c332ea9ee3c1a2f
Atanda1 <atandadave@gmail.com>
2023-09-11 23:34:58 +0100

    fix: endpoint

--- src/store/project/index.js
+++ src/store/project/index.js
@@ -10,17 +10,16 @@ export const projectStore = persist(
       let responseAbi;
       try {
         const responseAbi = await loadContractSpec(
-          process.env.REACT_APP__LOAD,
+          process.env.REACT_APP_CONTRACT_SPEC,
           address,
           chainId
         );
-
         if (responseAbi == null) {
-          
+          toast("Cannot retrieve verified contract. Try adding your ABI");
         }
-        
+        return responseAbi;
       } catch (error) {
-       
+        console.log(error);
         toast(`Confirm your Cannot retrieve verified contract. ${error}`);
       }
       return responseAbi;
@@ -44,9 +43,16 @@ export const projectStore = persist(
         verified !== true ? abi : await get().readContract(address, chainId);
       try {
         const project = new Collection(projectName, description);
-        
+        console.log(
+          verified,
+          contractName,
+          address,
+          chainId,
+          jsonRpcUrl,
+          contractAbi
+        );
 
-        
+        console.log(project);
         project.addContract({
           name: contractName,
           address,
@@ -57,20 +63,16 @@ export const projectStore = persist(
           inputData: {},
         });
 
-        
+        console.log(project);
+        console.log(project.displayData);
+        console.log(project.getSnapshot());
         set({
           collection: project,
         });
-        
+        console.log(Collection.fromData(project.getSnapshot()));
         history.navigate("/editor/method");
       } catch (err) {
-        toast(
-          `${
-            contractAbi === undefined
-              ? "Cannot retrieve verified contract. Try adding your ABI manually."
-              : err
-          }`
-        );
+        toast(`${err}`);
       }
     },
   }),
@@ -81,24 +83,25 @@ export const projectStore = persist(
         if (name !== "collection") {
           throw new Error();
         }
-       
+        console.log("CHEAP OPERATION");
+        console.log(newValue.state.collection);
 
         const snapshot = newValue.state.collection.getSnapshot();
-       
+        console.log();
         localStorage.setItem(name, JSON.stringify(snapshot));
       },
       getItem: async (name) => {
         if (name !== "collection") {
           throw new Error();
         }
-       
+        console.log("EXPENSIVE OPERATION");
         const str = localStorage.getItem(name);
         if (!str) {
           return null;
         }
 
         const collection = Collection.fromData(JSON.parse(str));
-        
+        console.log(collection.displayData);
 
         return {
           state: {
